a86326b329220e689ca87217b59a20b035b84ac2
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-06-25 10:51:52 +0530

    Basic Applciation Setup

---
+++ RestControllerAdvice/src/main/java/com/example/RestControllerAdvice/RestControllerAdviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestControllerAdvice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestControllerAdviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestControllerAdviceApplication.class, args);
+	}
+
+}
---
+++ RestControllerAdvice/src/test/java/com/example/RestControllerAdvice/RestControllerAdviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestControllerAdvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestControllerAdviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
